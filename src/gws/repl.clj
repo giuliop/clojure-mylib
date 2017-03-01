c0622f1fc20314a2b77ff690e4e9b878f0632d21
giulio <giuliop@gmail.com>
2017-03-01 22:01:50 +0100

    added the reset-ns function

--- src/gws/repl.clj
+++ src/gws/repl.clj
@@ -7,3 +7,13 @@
 
 (defn print-classpath []
   (doseq [x (classpath)] (println (. x getPath))))
+
+(defn reset-ns
+  ([]
+   (reset-ns *ns*))
+  ([_ns]
+   (in-ns 'user)
+   (remove-ns _ns)
+   (require _ns :reload)
+   (in-ns _ns)))
+
